637a43b3c18da94181bc2c2617ffdb42f44f07d3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-20 21:23:42 +0200

    Moved EntityDetails logic outiside the component into Movie and Person

--- src/features/DetailsPage/components/Movie/index.tsx
+++ src/features/DetailsPage/components/Movie/index.tsx
@@ -1,6 +1,56 @@
-import { apiEntityPathSegments } from "../../../../common/constants/apiEndpointPaths";
-import { EntityDetails } from "../EntityDetails";
+import { useParams } from "react-router-dom";
+import { Main } from "../../../../common/components/Main";
+import { useCombinedFetchStatus } from "../../../../common/hooks/useCombinedFetchStatus";
+import { useFetchGenres } from "../../../../common/hooks/useFetchGenres";
+import { Credits } from "../EntityDetails/Credits";
+import { HorizontalTile } from "../EntityDetails/HorizontalTile";
+import { MovieBanner } from "./Banner";
+import { MovieCredits } from "../../types/credits.types";
+import { DetailedMovieItem } from "../../../../common/aliases/interfaces/movie.types";
+import { useEntityDetails } from "../../../../common/hooks/useEntityDetails";
+import { useEntityCredits } from "../../../../common/hooks/useEntityCredits";
+import { entitiesSingularTypes } from "../../../../common/constants/entityTypes";
 
-export const Movie = () => (
-    <EntityDetails entityPathSegment={apiEntityPathSegments.movie} />
-);
+export const Movie = () => {
+    const { id } = useParams();
+    const fetchDependencies = [id!];
+
+    const genresStatus = useFetchGenres();
+
+    const {
+        status: movieStatus,
+        data: movie,
+        isPaused: isMoviePaused,
+    } = useEntityDetails<DetailedMovieItem>({ entityId: id!, entityType: entitiesSingularTypes.MOVIE, fetchDependencies });
+
+    const {
+        status: movieCreditsStatus,
+        data: movieCredits,
+        isPaused: isMovieCreditsPaued,
+    } = useEntityCredits<MovieCredits>({ entityId: id!, entityType: entitiesSingularTypes.PERSON, fetchDependencies });
+
+    const combinedFetchStatus = useCombinedFetchStatus(
+        [movieStatus, genresStatus, movieCreditsStatus],
+        [isMoviePaused, isMovieCreditsPaued]
+    );
+
+    return (
+        <>
+            <Main
+                successContent={
+                    <>
+                        <HorizontalTile entityDetails={movie} />
+                        <Credits credists={movieCredits} />
+                    </>
+                }
+                combinedFetchStatus={combinedFetchStatus}
+                errorMessage="Details not found"
+                bannerElement={
+                    <MovieBanner
+                        movieDetails={movie}
+                    />
+                }
+            />
+        </>
+    );
+};
--- src/features/DetailsPage/components/Person/index.tsx
+++ src/features/DetailsPage/components/Person/index.tsx
@@ -1,6 +1,48 @@
-import { apiEntityPathSegments } from "../../../../common/constants/apiEndpointPaths";
-import { EntityDetails } from "../EntityDetails";
+import { useParams } from "react-router-dom";
+import { PersonCredits } from "../../types/credits.types";
+import { useEntityCredits } from "../../../../common/hooks/useEntityCredits";
+import { useEntityDetails } from "../../../../common/hooks/useEntityDetails";
+import { useCombinedFetchStatus } from "../../../../common/hooks/useCombinedFetchStatus";
+import { HorizontalTile } from "../EntityDetails/HorizontalTile";
+import { Credits } from "../EntityDetails/Credits";
+import { Main } from "../../../../common/components/Main";
+import { entitiesSingularTypes } from "../../../../common/constants/entityTypes";
+import { DetailedPersonItem } from "../../../../common/aliases/interfaces/person.types";
 
-export const Person = () => (
-    <EntityDetails entityPathSegment={apiEntityPathSegments.person} />
-);
+export const Person = () => {
+    const { id } = useParams();
+
+    const fetchDependencies = [id!];
+
+    const {
+        status: personStatus,
+        data: person,
+        isPaused: isPersonPaused,
+    } = useEntityDetails<DetailedPersonItem>({ entityId: id!, entityType: entitiesSingularTypes.MOVIE, fetchDependencies });
+
+    const {
+        status: personFilmographyStatus,
+        data: personFilmography,
+        isPaused: isPersonFilmographyPaused,
+    } = useEntityCredits<PersonCredits>({ entityId: id!, entityType: entitiesSingularTypes.PERSON, fetchDependencies });
+
+    const combinedFetchStatus = useCombinedFetchStatus(
+        [personStatus, personFilmographyStatus],
+        [isPersonPaused, isPersonFilmographyPaused]
+    );
+
+    return (
+        <>
+            <Main
+                successContent={
+                    <>
+                        <HorizontalTile entityDetails={person} />
+                        <Credits credists={personFilmography} />
+                    </>
+                }
+                combinedFetchStatus={combinedFetchStatus}
+                errorMessage="Details not found"
+            />
+        </>
+    );
+};
